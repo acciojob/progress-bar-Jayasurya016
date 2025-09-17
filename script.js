416ddad2a4b16f7c20315ad0428dd766e09a626d
JAYASURYA KADAKUZHILL JOB <161327970+Jayasurya016@users.noreply.github.com>
2025-09-17 22:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,68 +1,54 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  const next = document.getElementById("next");
+  const prev = document.getElementById("prev");
+  const progress = document.getElementById("progress");
+  const circles = document.querySelectorAll(".circle");
 
-const next = document.getElementById("next");
-const prev = document.getElementById("prev");
-const progress = document.getElementById("progress");
-const circles = document.getElementById(".circle");
+  let currentActive = 1;
 
-let currentActive = 1;
+  next.addEventListener("click", () => {
+    currentActive++;
 
-next.addEventListener("click", ()=>{
-	currentActive++;
+    if (currentActive > circles.length) {
+      currentActive = circles.length;
+    }
 
-	if(currentActive > circles.length)
-	{
-		currentActive = circles.length;
-	}
-	update();
+    update();
+  });
+
+  prev.addEventListener("click", () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+      currentActive = 1;
+    }
+
+    update();
+  });
+
+  function update() {
+    circles.forEach((circle, idx) => {
+      if (idx < currentActive) {
+        circle.classList.add("active");
+      } else {
+        circle.classList.remove("active");
+      }
+    });
+
+    const actives = document.querySelectorAll(".circle.active");
+    progress.style.width =
+      ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+    if (currentActive === 1) {
+      prev.disabled = true;
+    } else if (currentActive === circles.length) {
+      next.disabled = true;
+    } else {
+      prev.disabled = false;
+      next.disabled = false;
+    }
+  }
+
+  // Initial call to set the correct state on page load
+  update();
 });
-
-prev.addEventListener("click", ()=>{
-	currentActive--;
-
-	if (currentActive < 1) {
-		currentActive = 1;		
-	}
-	update();
-});
-
-function update() {
-		circles.forEach((circles, idx)=>{
-			if (idx < currentActive) {
-				circles.classList.add("active");
-			}else{
-				circles.classList.remove("active");
-			}
-		});
-
-	const active = document.querySelectorAll(".circle.active");
-	progress.style.width = ((active.length-1)/ (circles.length-1)) * 100 + "%";
-
-		if (currentActive === 1) {
-		prev.disabled = true;
-	}else if (currentActive === circles.length) {
-		next.disabled = true;
-	}else {
-			prev.disabled = false;
-			next.disabled = false;
-	}
-}
-
-update();
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
